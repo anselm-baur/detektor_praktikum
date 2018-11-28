67d2c8eeaf37abf176dd2fc7444f49bed4c0492a
Anselm Baur <anselm.baur@live.de>
2018-11-28 11:14:16 +0100

    still initial

--- start_measuring.sh
+++ start_measuring.sh
@@ -17,15 +17,13 @@ fi
 
 
 for ((NUM_MEASURE=0; NUM_MEASURE<3; NUM_MEASURE++))
-#PXAR_UP="cat xray | ./pxar/bin/pXar -d pxar/output/${BOARD_UP}/ -T 35 -r rootfiles/measuring/meas_${NUM_MEASURE}.root"
-#	PXAR_DOWN="cat xray | ./pxar/bin/pXar -d pxar/output/${BOARD_UP}/ -T 35 -r rootfiles/measuring/meas_${NUM_MEASURE}.root"
+	PXAR_UP="cat xray | ./pxar/bin/pXar -d pxar/output/${BOARD_UP}/ -T 35 -r rootfiles/measuring/meas_${NUM_MEASURE}.root"
+	PXAR_DOWN="cat xray | ./pxar/bin/pXar -d pxar/output/${BOARD_UP}/ -T 35 -r rootfiles/measuring/meas_${NUM_MEASURE}.root"
 	do
-		stdbuf -o0 ./test.sh > $LOG_UP &
-#./test > log_1 &
+		stdbuf -o0 ${PXAR_UP} > $LOG_UP &
 		pid_1=$!
 
-		stdbuf -o0 ./test.sh > $LOG_DOWN &
-#./test > log_2 &
+		stdbuf -o0 ${PXAR_DOWN} > $LOG_DOWN &
 		pid_2=$!
 		
 		echo ""
@@ -35,20 +33,20 @@ for ((NUM_MEASURE=0; NUM_MEASURE<3; NUM_MEASURE++))
 
 		echo "Starting Board ${BOARD_UP} @PID: $pid_1           Starting Board ${BOARD_DOWN} @PID: $pid_2"
 		while ps $pid_1>/dev/null || ps $pid_2>/dev/null; do
-			OUTPUT_1=""
-			OUTPUT_2=""
+			OUTPUT_UP=""
+			OUTPUT_DOWN=""
 
-			OUTPUT_1=$(tail -2 $LOG_UP | head -1 | grep DEBUGAPI)
-			if [ -z $OUTPUT_1 ]; then
-				OUTPUT_1=$(tail -2 $LOG_UP | head -1)
+			OUTPUT_UP=$(tail -2 $LOG_UP | head -1 | grep DEBUGAPI)
+			if [ -z $OUTPUT_UP ]; then
+				OUTPUT_UP=$(tail -2 $LOG_UP | head -1)
 			fi
 
-			OUTPUT_2=$(tail -2 $LOG_DOWN | head -1 | grep DEBUGAPI)
-			if [ -z $OUTPUT_2 ]; then
-				OUTPUT_2=$(tail -2 $LOG_DOWN | head -1)
+			OUTPUT_DOWN=$(tail -2 $LOG_DOWN | head -1 | grep DEBUGAPI)
+			if [ -z $OUTPUT_DOWN ]; then
+				OUTPUT_DOWN=$(tail -2 $LOG_DOWN | head -1)
 			fi
 
-			echo "Board ${BOARD_UP}: $OUTPUT_1         Board ${BOARD_DOWN}: $OUTPUT_2"
+			echo "Board ${BOARD_UP}: $OUTPUT_UP         Board ${BOARD_DOWN}: $OUTPUT_DOWN"
 			sleep 0.1
 		done
 	done
