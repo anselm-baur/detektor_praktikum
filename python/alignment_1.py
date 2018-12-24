4bf03a2679ec6018bfc47b54c98ed24a3ccb87dc
Anselm Baur <anselm.baur@live.de>
2018-12-24 18:02:13 +0100

    alignement fit done, table in with fit parameters in protokoll

---
+++ python/alignment_1.py
@@ -0,0 +1,170 @@
+import ROOT
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import utils
+from scipy.optimize import curve_fit
+
+upper_board = "m4587"
+lower_board = "m4520"
+lower_aligenment_raw_data = "../data/{}/alignment_1.root".format(lower_board)
+upper_aligenment_raw_data = "../data/{}/alignment_1.root".format(upper_board)
+#lower_aligenment_raw_data = "alignment_1.root"
+#root_file = ROOT.TFile.Open(lower_aligenment_raw_data)
+
+root_file = ROOT.TFile.Open(upper_aligenment_raw_data)
+
+	
+#hist2 = root_file.Get('Xray/hMap_Ag_C8_V0')
+#canvas = ROOT.TCanvas()
+
+#hist1.Draw("COLZ")
+#hist2.Draw("COLZ")
+#canvas.Draw()
+
+X_PIXEL = 52
+Y_PIXEL = 80
+X_SIZE = int(8*8100)
+Y_SIZE = int(2*8100)
+
+x_hist_upper = np.zeros(X_SIZE)
+y_hist_upper = np.zeros(Y_SIZE)
+
+x_hist_lower = np.zeros(X_SIZE)
+y_hist_lower = np.zeros(Y_SIZE)
+
+data_matrix = np.zeros((X_SIZE, Y_SIZE))
+
+with utils.ShutUpRoot():
+	for i in range (0,16):
+		hist_name = 'Xray/hMap_Ag_C{}_V0'.format(i)
+		print("load hist: {}".format(hist_name))
+		root_hist = root_file.Get(hist_name)
+		for x in range(1,X_PIXEL+1):
+			for y in range(1,Y_PIXEL+1):
+	
+				event = utils.transform_into_xy(np.array([[i],[x-1],[y-1],[1]]), 1)
+				if (y == 1 or y == Y_PIXEL) or (x == 1 or x == X_PIXEL):
+	#data_matrix[x-1][y-1] = hist1.GetCellContent(x, y)/2
+					x_hist_upper[int(event[0])] += root_hist.GetCellContent(x,y)/2
+					y_hist_upper[int(event[1])] += root_hist.GetCellContent(x,y)/2
+				
+				else:
+	#      	data_matrix[x-1][y-1] = hist1.GetCellContent(x, y)
+					x_hist_upper[int(event[0])] += root_hist.GetCellContent(x, y)
+					y_hist_upper[int(event[1])] += root_hist.GetCellContent(x, y)
+	
+	
+	root_file = ROOT.TFile.Open(lower_aligenment_raw_data)
+	
+	for i in range (0,16):
+		hist_name = 'Xray/hMap_Ag_C{}_V0'.format(i)
+		print("load hist: {}".format(hist_name))
+		root_hist = root_file.Get(hist_name)
+		for x in range(1,X_PIXEL+1):
+			for y in range(1,Y_PIXEL+1):
+	
+				event = utils.transform_into_xy(np.array([[i],[x-1],[y-1],[1]]), 2)
+				if (y == 1 or y == Y_PIXEL) or (x == 1 or x == X_PIXEL):
+	#data_matrix[x-1][y-1] = hist1.GetCellContent(x, y)/2
+					x_hist_lower[int(event[0])] += root_hist.GetCellContent(x,y)/2
+					y_hist_lower[int(event[1])] += root_hist.GetCellContent(x,y)/2
+				
+				else:
+	#      	data_matrix[x-1][y-1] = hist1.GetCellContent(x, y)
+					x_hist_lower[int(event[0])] += root_hist.GetCellContent(x, y)
+					y_hist_lower[int(event[1])] += root_hist.GetCellContent(x, y)
+	
+	
+#print(data_matrix[30][50])
+
+#input("eingabe: ")
+root_file.Close()
+
+fig = plt.figure()
+ax = fig.add_subplot(121)#, projection='3d')
+ay = fig.add_subplot(122)
+#ax.bar3d(data_matrix)
+x_upper_select = x_hist_upper!=0
+x_lower_select = x_hist_lower!=0
+y_upper_select = y_hist_upper!=0
+y_lower_select = y_hist_lower!=0
+
+
+
+# Fit environment for x direction
+x_min = 30000
+x_max = X_SIZE
+
+# upper board
+x_fit_select = x_hist_upper[x_min:x_max]!= 0 # filter out bins where zero entries
+x_fit_hist = x_hist_upper[x_min:x_max] # histogramm contains just the bins with entries
+popt, pcov = curve_fit(utils.gauss, np.array(range(x_min,x_max))[x_fit_select], x_fit_hist[x_fit_select], p0=[30000, 55000, 5000], bounds=([30000, 50000, 1000],[45000,55000,10000]), maxfev=10000)
+print("x upper board:")
+print(popt)
+A_up = popt[0]
+mu_up = popt[1]
+sigma_up = popt[2]
+
+# lower board
+x_fit_select = x_hist_lower[x_min:x_max]!= 0 # filter out bins where zero entries
+x_fit_hist = x_hist_lower[x_min:x_max] # histogramm contains just the bins with entries
+popt, pcov = curve_fit(utils.gauss, np.array(range(x_min,x_max))[x_fit_select], x_fit_hist[x_fit_select], p0=[5000, 50000, 10000])
+print("x lower board:")
+print(popt)
+A_low = popt[0]
+mu_low = popt[1]
+sigma_low = popt[2]
+
+
+
+
+# Fit environment for y direction
+y_min = 0
+y_max = Y_SIZE
+
+#upper board
+y_fit_select = y_hist_upper[y_min:y_max]!= 0
+y_fit_hist = y_hist_upper[y_min:y_max]
+popt, pcov = curve_fit(utils.gauss, np.array(range(y_min,y_max))[y_fit_select], y_fit_hist[y_fit_select], p0=[25000, 8000, 10000])
+print("y upper board:")
+print(popt)
+A_yup = popt[0]
+mu_yup = popt[1]
+sigma_yup = popt[2]
+
+#lower board
+y_fit_select = y_hist_lower[y_min:y_max]!= 0
+y_fit_hist = y_hist_lower[y_min:y_max]
+popt, pcov = curve_fit(utils.gauss, np.array(range(y_min,y_max))[y_fit_select], y_fit_hist[y_fit_select], p0=[8000, 8000, 10000])
+print("y lower board:")
+print(popt)
+A_ylow = popt[0]
+mu_ylow = popt[1]
+sigma_ylow = popt[2]
+
+
+##########################
+# TEX				     #
+##########################
+print("{:.0f} & {:.0f} &  {:.0f} & {:.0f} & {:.0f} & {:.0f} \\\\".format(A_up, mu_up, sigma_up, A_yup, mu_yup, sigma_yup))
+print("{:.0f} & {:.0f} &  {:.0f} & {:.0f} & {:.0f} & {:.0f} \\\\".format(A_low, mu_low, sigma_low, A_ylow, mu_ylow, sigma_ylow))
+
+ax.plot(np.array(range(0,X_SIZE))[x_upper_select]/1000, x_hist_upper[x_upper_select], 'b.', alpha=0.5, label="upper")
+ax.plot(np.array(range(0,X_SIZE))[x_lower_select]/1000, x_hist_lower[x_lower_select], 'r.', alpha=0.5, label="lower")
+ax.plot(np.arange(0,X_SIZE)/1000, utils.gauss(range(0,X_SIZE), A_up, mu_up, sigma_up), 'b-', label="fit_upper")
+ax.plot(np.arange(0,X_SIZE)/1000, utils.gauss(range(0,X_SIZE), A_low, mu_low, sigma_low), 'r-', label="fit_upper")
+ax.set_xlabel("x position in mm")
+ax.set_ylabel("intensity in arbitrary unit")
+
+
+
+ay.plot(np.array(range(0,Y_SIZE))[y_upper_select]/1000, y_hist_upper[y_upper_select], 'b.', alpha=0.5, label="upper")
+ay.plot(np.array(range(0,Y_SIZE))[y_upper_select]/1000, y_hist_lower[y_lower_select], 'r.', alpha=0.5, label="lower")
+ay.plot(np.arange(0,Y_SIZE)/1000, utils.gauss(range(0,Y_SIZE), A_yup, mu_yup, sigma_yup), 'b-', label="fit_upper")
+ay.plot(np.arange(0,Y_SIZE)/1000, utils.gauss(range(0,Y_SIZE), A_ylow, mu_ylow, sigma_ylow), 'r-', label="fit_lower")
+ay.set_xlabel("y position in mm")
+
+plt.legend()
+plt.show()
+
---
+++ python/analyse.py
@@ -0,0 +1,132 @@
+import ROOT
+import numpy as np
+import root_numpy as rn
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+from ROOT import TVector3
+
+# define some global variables values in µm
+x_chip = 8100
+y_chip = 8100
+# chip
+delta_x_chip = np.full(52,150)
+delta_x_chip[0] = 300
+delta_x_chip[51] = 300
+
+delta_y_chip = np.full(160,100)
+delta_y_chip[79] = 200
+delta_y_chip[80] = 200
+
+delta_z = -0.01 * 10**6
+
+# function to read a root file and return the needed information as a list of tuples (chip,column,row,weight)
+def read_root(board1,board2,i):
+    f_up = ROOT.TFile.Open('../data/{}/measuring1/meas_{}.root'.format(board1,i),'read')
+    f_down = ROOT.TFile.Open('../data/{}/measuring1/meas_{}.root'.format(board2,i),'read')
+    tree_up = f_up.Get('Xray/events')
+    tree_down = f_down.Get('Xray/events')
+
+    up_proc_array = rn.tree2array(tree_up, branches=['proc'])
+    up_pcol_array = rn.tree2array(tree_up, branches=['pcol'])
+    up_prow_array = rn.tree2array(tree_up, branches=['prow'])
+    up_pq_array = rn.tree2array(tree_up, branches=['pq'])
+
+    down_proc_array = rn.tree2array(tree_down, branches=['proc'])
+    down_pcol_array = rn.tree2array(tree_down, branches=['pcol'])
+    down_prow_array = rn.tree2array(tree_down, branches=['prow'])
+    down_pq_array = rn.tree2array(tree_down, branches=['pq'])
+
+    events_up = []
+    events_down = []
+
+    if len(up_proc_array) != len(down_proc_array):
+        print('WARNING: Not the same number of events!')
+        return events_up, events_down
+
+    #if len(up_proc_array) != len(up_pcol_array) or len(up_proc_array) != len(up_prow_array) or len(up_proc_array) != len(up_pq_array):
+    #    print('ups')
+    #    return events
+
+
+    for i in range(len(up_proc_array)):
+        if up_proc_array[i][0] != [] and len(up_proc_array[i][0]) < 5 and down_proc_array[i][0] != [] and len(down_proc_array[i][0]) < 5:
+            events_up.append((list(up_proc_array[i][0]),list(up_pcol_array[i][0]),list(up_prow_array[i][0]),list(up_pq_array[i][0])))
+            events_down.append((list(down_proc_array[i][0]), list(down_pcol_array[i][0]), list(down_prow_array[i][0]),list(down_pq_array[i][0])))
+
+    return events_up, events_down
+
+# upper board = 1, lower = 2
+def transform_into_xy(event,board=1):
+    chips = np.array(event[0])
+    cols = np.array(event[1])
+    rows = np.array(event[2])
+    weights = np.array(event[3])
+    x_weighted = np.zeros(len(chips))
+    y_weighted = np.zeros(len(chips))
+
+    for i in range(len(chips)):
+        chip = chips[i]
+        col = cols[i]
+        row = rows[i]
+        if chip > 7:
+            chip = 15 - chip
+            col = 51 - col
+            row = 79 + 80 - row
+        # transform col into x
+        x_weighted[i] = (chip * x_chip + np.sum(delta_x_chip[:col]) + 0.5 * delta_x_chip[col]) * weights[i]
+
+        # transform row into y
+        y_weighted[i] = (np.sum(delta_y_chip[:row]) + 0.5 * delta_y_chip[row]) * weights[i]
+
+    # use weight to find center
+    x = np.sum(x_weighted) / np.sum(weights)
+    y = np.sum(y_weighted) / np.sum(weights)
+
+    if board == 1:
+        return TVector3(x,y,0)
+    else:
+        return TVector3(x,2*y_chip-y,delta_z)
+
+def fit_2(hit_1, hit_2):
+    s = hit_1
+    t = hit_2-hit_1
+    return [s + x * t for x in range(-5,5)]
+
+for i in range(0,5):
+    events_up, events_down = read_root('m4587','m4520',i)
+    hits_up = []
+    hits_down = []
+    fits = []
+
+    for i in range(len(events_up)):
+    #for event in events_up:
+        hit_up = transform_into_xy(events_up[i])
+        hits_up.append(hit_up)
+
+        hit_down = transform_into_xy(events_down[i],board=2)
+        hits_down.append(hit_down)
+
+        fits.append(fit_2(hit_up,hit_down))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot([x.X() for x in hits_up],[x.Y() for x in hits_up],[x.Z() for x in hits_up],'bs')
+    ax.plot([x.X() for x in hits_down],[x.Y() for x in hits_down],[x.Z() for x in hits_down],'r*')
+    for fit in fits:
+        ax.plot([x.X() for x in fit], [x.Y() for x in fit], [x.Z() for x in fit], 'black')
+
+    x_surf = np.arange(-0, 8*8100, 1000)
+    y_surf = np.arange(-0, 2*8100, 1000)
+    x_surf, y_surf = np.meshgrid(x_surf, y_surf)
+    z_surf = np.zeros(x_surf.shape)
+    ax.plot_surface(x_surf, y_surf, z_surf, color='b',alpha=0.4)
+    ax.plot_surface(x_surf, y_surf, z_surf+delta_z, color='r', alpha=0.4)
+
+
+    plt.ylim(ymax=-100, ymin=16300)
+    plt.xlim(xmax=-100, xmin=8*8100+100)
+    ax.set_zlim(delta_z-100,100)
+    ax.grid(False)
+    plt.show()
+
